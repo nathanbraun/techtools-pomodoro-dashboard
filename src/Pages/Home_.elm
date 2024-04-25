65af5c05fd61eb5e3c844bbd98bb7a0bbd56ab4f
Nathan Braun <nathanericbraun@gmail.com>
2024-04-25 09:12:12 -0500

    feat: View uses elm-tailwind-modules
    feat: update home

--- src/Pages/Home_.elm
+++ src/Pages/Home_.elm
@@ -1,11 +1,12 @@
 module Pages.Home_ exposing (page)
 
 import Html
+import Html.Styled as Html exposing (..)
 import View exposing (View)
 
 
 page : View msg
 page =
     { title = "Homepage"
-    , body = [ Html.text "Hello, world!" ]
+    , body = [ text "Hello, world!" ]
     }
